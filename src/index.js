526fb7d0c4334837219ffde1714ebd65f3f580c5
Lee Ryan <2solimanleeryan@gmail.com>
2022-10-29 12:56:49 +0800

    Add nav bar to content

--- src/index.js
+++ src/index.js
@@ -19,12 +19,23 @@ function createTitle(title) {
 function createContentSpace() {
     const main = document.createElement('main');
     main.id = 'content';
-    // main.appendChild(createNavbar());
+    main.appendChild(createNavbar());
     return main;
 }
 
 function createNavbar() {
-
+    let navItems = ['Home', 'About', 'Contact Information'];
+    const nav = document.createElement('nav');
+    nav.id = 'nav-bar';
+    const ul = document.createElement('ul');
+    navItems.forEach((item) => {
+        let navItem = document.createElement('li');
+        navItem.textContent = item;
+        if(item == 'Home') navItem.classList.add('active');
+        ul.appendChild(navItem);
+    });
+    nav.appendChild(ul);
+    return nav;
 }
 
 document.body.appendChild(setPageBase());
